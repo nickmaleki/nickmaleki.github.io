c58caee3bce367ae22638542cd99e9f68e0e80ea
nickmaleki <47644227+nickmaleki@users.noreply.github.com>
2020-05-25 17:48:57 -0400

    Updates

---
+++ precache-manifest.a1723b7f2242e212e661f00595c68889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58953a0613ca7ec7d3e35eb2384f54a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae344631a128df5ea49",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96242d6f52769696c451",
+    "url": "/static/js/2.dc4b2c19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc4b2c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae344631a128df5ea49",
+    "url": "/static/js/main.286a628a.chunk.js"
+  },
+  {
+    "revision": "b36a953cf4e0906a9f41",
+    "url": "/static/js/runtime-main.de3c6d54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

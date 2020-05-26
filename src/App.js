d8ba872a8708c8527c69c807b61aba5266e7c5ba
nickmaleki <47644227+nickmaleki@users.noreply.github.com>
2020-05-26 11:07:49 -0400

    added react default screen

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and egg to reload.
+          Edit <code>src/App.js</code> and Egg to reload.
         </p>
         <a
           className="App-link"

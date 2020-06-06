db6ddf9c3ef102f2c7ea3f6d224f7a4c58796841
nickmaleki <47644227+nickmaleki@users.noreply.github.com>
2020-06-05 21:41:51 -0400

    Added particle system and clickable icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faTwitter, } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+
+document.title = "Nick Maleki";
+
+function handleLinkedin() {
+  window.open("https://www.linkedin.com/in/nicholas-maleki/", '_blank');
+}
+
+function handleGithub() {
+  window.open("https://github.com/nickmaleki", '_blank');
+}
+
+function handleMail() {
+  window.open("mailto:nicholasmaleki@gmail.com", '_blank');
+}
+
+function handleTwitter() {
+  window.open("https://twitter.com/malekinick", '_blank');
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and Egg to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="navi">
+      <Particles
+        params={{
+          particles: {
+            opacity: 1,
+            color: "#ed7e48",
+            number: {
+              value: 100
+            },
+            size: {
+              value: 5
+            },
+          },
+          background: {
+            image: "linear-gradient(135deg, #ffffff -100%, #093f79 50%)"
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse"
+              }
+            }
+          }
+        }}
+      >
+      </Particles>
+
+      <div className="mainflex">
+        <h1 className="centered" style={{top: '-55px', color: '#ffffff' }}>Nick Maleki</h1>
+        <ul id="nav-links">
+          <li>
+            <FontAwesomeIcon className="icon" icon={faLinkedin} color="#ffffff" size="2x" onClick={handleLinkedin} />
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faGithub} color="#ffffff" size="2x" onClick={handleGithub} />
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faEnvelope} color="#ffffff" size="2x" onClick={handleMail} />
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faTwitter} color="#ffffff" size="2x" onClick={handleTwitter} />
+          </li>
+        </ul>
+      </div>
+
     </div>
   );
 }
